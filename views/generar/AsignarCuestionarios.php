fe662d0e6d61577df30c510546fd06d3dad85fe4
Dennis Ponce <dennis-ponce07@outlook.com>
2024-07-22 03:52:01 -0500

    cambios

--- views/generar/AsignarCuestionarios.php
+++ views/generar/AsignarCuestionarios.php
@@ -5,40 +5,52 @@
                 Aqui vamos a tener todos nuestros usaurios el cual les vamos ir asignando un formulario... 
 
             -->
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Nombres</th>
-                        <th>Correo</th>
-                        <th>Rol</th>
-                        <th>Asignacion Formulario</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach($res as $key=>$v){  ?>
-                    <tr>
-                        <td><?php echo $v["usuario_id"]; ?></td>
-                        <td><?php echo $v["usuario_nombre"] . $v["usuario_apellido"]; ?></td>
-                        <td><?php echo $v["usuario_email"]; ?></td>
-                        <td><?php echo $v["rol_nombre"]; ?></td>
-                        <td>
-                            <form method="POST" action="?accion=generarAsignacionUsuario">
-                                <input type="hidden" name="responsable_<?php echo $v["usuario_id"]; ?>_asignacion_usuarioIdAsignado" value="<?php echo $v["usuario_id"];  ?>"/>
-                                <label for="Cuestionario">Asigna un cuestionario</label>
-                                <select name="responsable_<?php echo $v["usuario_id"]; ?>_asignacion_TipoCuestionarioId" id="Cuestionario">
-                                    <option selected="true" value="0" disabled> Asignar Cuestionario </option>
-                                    <?php foreach($cues as $ks=>$vs) {  ?>
-                                        <option value="<?php echo $vs["tipoCues_id"]; ?>"><?php echo $vs["tipoCues_tema"]; ?></option>
-                                    <?php } ?>
-                                </select>
-                                <input type="submit" value="Asignar cuestionario" name="responsable_<?php echo $v["usuario_id"]; ?>"/>
-                            </form>
-                        </td>
-                    </tr>
-                    <?php } ?>
-                </tbody>
-            </table>
+            <div class="proceso">
+                <div class="descripcion">
+                    <h1 class="text-center p-3">Asignacion Cuestionarios</h1>
+                    <p class="fw-lighter text-center">Ahora puedes asignar los cuestionarios a tus usuarios</p>
+                </div>                
+                <div class="panel mx-5">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th class="text-center"><i class="bi bi-dot"></i> Id</th>
+                                <th class="text-center"><i class="bi bi-person-fill"></i> Nombres</th>
+                                <th class="text-center"><i class="bi bi-envelope-check-fill"></i> Correo</th>
+                                <th class="text-center"><i class="bi bi-person-rolodex"></i> Rol</th>
+                                <th class="text-center"><i class="bi bi-file-ruled-fill"></i> Asignacion Formulario</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach($res as $key=>$v){  ?>
+                            <tr>
+                                <td style="vertical-align: middle; font-size:1.5rem;" class="text-center fw-lighter" scope="col"><?php echo $v["usuario_id"]; ?></td>
+                                <td style="vertical-align: middle; font-size:1.2rem;" class="text-center fw-lighter"><?php echo $v["usuario_nombre"] . $v["usuario_apellido"]; ?></td>
+                                <td style="vertical-align: middle; font-size:1.2rem;" class="text-center fw-lighter"><?php echo $v["usuario_email"]; ?></td>
+                                <td style="vertical-align: middle; font-size:1.2rem;" class="text-center fw-lighter">
+                                    <p class="btn" style="background-color: #30ea2a; color:#fff;">
+                                        <?php echo $v["rol_nombre"]; ?>
+                                    </p>
+                                </td>
+                                <td> 
+                                    <form method="POST" action="?accion=generarAsignacionUsuario">
+                                        <input type="hidden" name="responsable_<?php echo $v["usuario_id"]; ?>_asignacion_usuarioIdAsignado" value="<?php echo $v["usuario_id"];  ?>"/>
+                                        <label for="Cuestionario" class="fw-lighter py-2">Asigna un cuestionario</label>
+                                        <select class="form-control" name="responsable_<?php echo $v["usuario_id"]; ?>_asignacion_TipoCuestionarioId" id="Cuestionario">
+                                            <option selected="true" value="0" disabled> Asignar Cuestionario </option>
+                                            <?php foreach($cues as $ks=>$vs) {  ?>
+                                                <option value="<?php echo $vs["tipoCues_id"]; ?>"><?php echo $vs["tipoCues_tema"]; ?></option>
+                                            <?php } ?>
+                                        </select>
+                                        <input type="submit" value="Asignar cuestionario" class="btn btn-warning mt-2" name="responsable_<?php echo $v["usuario_id"]; ?>"/>
+                                    </form>
+                                </td>
+                            </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
 
         <!-- 
